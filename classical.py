fbb796e09fe994109829280f208524edf3826766
BelleST <458192674@qq.com>
2021-11-21 13:39:34 +0800

    rabin

--- classical.py
+++ classical.py
@@ -113,23 +113,42 @@ m,k
 #
 
 #polybius
-import itertools
+# import itertools
+#
+# key = []
+# cipher = "ouauuuoooeeaaiaeauieuooeeiea"
+#
+# for i in itertools.permutations('aeiou', 5):
+#     key.append(''.join(i))
+#
+# for now_key in key:
+#     solve_c = ""
+#     res = ""
+#     for now_c in cipher:
+#         solve_c += str(now_key.index(now_c))
+#     for i in range(0,len(solve_c),2):
+#         now_ascii = int(solve_c[i])*5+int(solve_c[i+1])+97
+#         if now_ascii>ord('i'):
+#             now_ascii+=1
+#         res += chr(now_ascii)
+#     if "flag" in res:
+#         print(now_key, res)
 
-key = []
-cipher = "ouauuuoooeeaaiaeauieuooeeiea"
 
-for i in itertools.permutations('aeiou', 5):
-    key.append(''.join(i))
+#移位
+fib = "今天上午，朝歌区梆子公司决定，在每天三更天不亮免费在各大小区门口设卡为全城提供二次震耳欲聋的敲更提醒，呼吁大家早睡早起，不要因为贪睡断送大好人生，时代的符号是前进。为此，全区老人都蹲在该公司东边树丛合力抵制，不给公司人员放行，场面混乱。李罗鹰住进朝歌区五十年了，人称老鹰头，几年孙子李虎南刚从东北当猎户回来，每月还寄回来几块鼹鼠干。李罗鹰当年遇到的老婆是朝歌一枝花，所以李南虎是长得非常秀气的一个汉子。李罗鹰表示：无论梆子公司做的对错，反正不能打扰他孙子睡觉，子曰：‘睡觉乃人之常情’。梆子公司这是连菩萨睡觉都不放过啊。李南虎表示：梆子公司智商捉急，小心居民猴急跳墙！这三伏天都不给睡觉，这不扯淡么！到了中午人群仍未离散，更有人提议要烧掉这个公司，公司高层似乎恨不得找个洞钻进去。直到治安人员出现才疏散人群归家，但是李南虎仍旧表示爷爷年纪大了，睡不好对身体不好。"
 
-for now_key in key:
-    solve_c = ""
-    res = ""
-    for now_c in cipher:
-        solve_c += str(now_key.index(now_c))
-    for i in range(0,len(solve_c),2):
-        now_ascii = int(solve_c[i])*5+int(solve_c[i+1])+97
-        if now_ascii>ord('i'):
-            now_ascii+=1
-        res += chr(now_ascii)
-    if "flag" in res:
-        print(now_key, res)
+f =   "喵天上午，汪歌区哞叽公司决定，在每天八哇天不全免费在各大小区门脑设卡为全城提供双次震耳欲聋的敲哇提醒，呼吁大家早睡早起，不要因为贪睡断送大好人生，时代的编号是前进。为此，全区眠人都足在该公司流边草丛合力抵制，不给公司人员放行，场面混乱。李罗鸟住进汪歌区五十年了，人称眠鸟顶，几年孙叽李熬值刚从流北当屁户回来，每月还寄回来几块报信干。李罗鸟当年遇到的眠婆是汪歌一枝花，所以李值熬是长得非常秀气的一个汉叽。李罗鸟表示：无论哞叽公司做的对错，反正不能打扰他孙叽睡觉，叽叶：‘睡觉乃人之常情’。哞叽公司这是连衣服睡觉都不放过啊。李值熬表示：哞叽公司智商捉急，小心居民猴急跳墙！这八伏天都不给睡觉，这不扯淡么！到了中午人群仍未离散，哇有人提议要烧掉这个公司，公司高层似乎恨不得找个洞钻进去。直到治安人员出现才疏散人群归家，但是李值熬仍旧表示爷爷年纪大了，睡不好对身体不好。"
+
+c = "喵汪哞叽双哇顶，眠鸟足屁流脑，八哇报信断流脑全叽，眠鸟进北脑上草，八枝遇孙叽，孙叽对熬编叶：值天衣服放鸟捉猴顶。鸟对：北汪罗汉伏熬乱天门。合编放行，卡编扯呼。人离烧草，报信归洞，孙叽找爷爷。"
+
+m = ['']*32
+
+fib = fib.split('')
+f = f.split('')
+print(fib)
+
+for i in range(len(f)):
+    m[fib.index(f[i])] = c[i]
+for i in m:
+    print(i,end='')
--- elgamal.py
+++ elgamal.py
@@ -1,28 +1,34 @@
 import gmpy2
-PK = 11496075768305337400615691305824078755552811953107530344393099165443081142499567126258295155973173746213213707843701733303043515285566768766635366352888223
-#求g、q、SK
-
-
-def decrypt(PK, SK, c):
-    p = PK
-    g, q = SK
-    g_inv = gmpy2.invert(g, p)
-
-    m = (c * pow(g_inv, 3, p) % p) % q
-    return m
-
-
-if __name__ == '__main__':
-    from secret import flag
-    assert flag.startswith('{') and flag.endswith('}')
-    assert len(flag) == 34
-
-    PK, SK = keygen()
-    raw_flag = flag[1:-1]
-
-    with open('data.txt', 'w') as f:
-        f.write("PK: %d\n" % PK)
-        for m in raw_flag:
-            bit = int(m, 2)
-            c = encrypt(PK, SK, bit)
-            f.write(str(c) + '\n')
+#离散对数
+import sage
+c = 6665851394203214245856789450723658632520816791621796775909766895233000234023642878786025644953797995373211308485605397024123180085924117610802485972584499
+m = 391190709124527428959489662565274039318305952172936859403855079581402770986890308469084735451207885386318986881041563704825943945069343345307381099559075
+n = 2**512
+x = discrete_log(c,mod(m,n))
+# PK = 11496075768305337400615691305824078755552811953107530344393099165443081142499567126258295155973173746213213707843701733303043515285566768766635366352888223
+# #求g、q、SK
+#
+#
+# def decrypt(PK, SK, c):
+#     p = PK
+#     g, q = SK
+#     g_inv = gmpy2.invert(g, p)
+#
+#     m = (c * pow(g_inv, 3, p) % p) % q
+#     return m
+#
+#
+# if __name__ == '__main__':
+#     from secret import flag
+#     assert flag.startswith('{') and flag.endswith('}')
+#     assert len(flag) == 34
+#
+#     PK, SK = keygen()
+#     raw_flag = flag[1:-1]
+#
+#     with open('data.txt', 'w') as f:
+#         f.write("PK: %d\n" % PK)
+#         for m in raw_flag:
+#             bit = int(m, 2)
+#             c = encrypt(PK, SK, bit)
+#             f.write(str(c) + '\n')
