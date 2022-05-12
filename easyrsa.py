b5877ee0fc04a365787f79cfe0573ac814c0c907
BelleST <458192674@qq.com>
2022-05-12 15:47:30 +0800

    reverse-pydes

--- easyrsa.py
+++ easyrsa.py
@@ -1,11 +1,50 @@
-n = 0x6cb77aaf0bdc8c89cb7874cfad460897a41199df8dcfc6b991715f59a8c264d699a5d40c16ccc7625fd95c7a53e47dc70294128196badb5fa520e557d2eef972dc948924bf5af879407e445b06cf12a3504392391fb59d9c71a89aa6afab8a7afe31a4ff4e6f6c190628599eb1019f16264aa77d8f44fcb780c60715eb7beec7
-e = 0x10001
-c = 0x1c88587ac5c541d47368fc03e054d30ef8d963896113daaa8c4ca8e0a95c5cbca72867301b86618ea86a758b011802bfe33597a4eaac6c7050cedbc671a49a44f584df8c1304616b5fa1a6af0e9c517caf94679eaba46015b486e329b59ab229960b728293fa5fb71157afe9762c29c88bdb06a1742ed1083a511d68996cf4ec
-sub = 0x535408bdf9e1c11d5b41867ede3d953bd33bccdc4adccf830a2ac8d0b66c12cfbc7bdee300559807d3c2f063d1c0af0970603c3afa4dd3ee8012fdcd8be02a56
-import math
-import Rsa
-for i in range(5,int(math.sqrt(n))):
-    if i*(i+sub) == n:
-        print("find!",i)
-        d = Rsa.get_d(e,i+sub,i)
-        m = Rsa.decrypt(e,c,d,n)
+from base64 import b64decode
+from itertools import product
+from DES import * 			 # https://github.com/soreatu/Cryptography/blob/master/DES.py
+
+
+guess_8bit = list(product(range(2), repeat=8))
+not_in_PC2 = [9,18,22,25,35,38,43,54]
+
+def re_PC2(sbkey):
+    # 48-bit -> 56-bit
+    res = [0]*56
+    for i in range(len(sbkey)):
+        res[PC_2_table[i]-1] = sbkey[i]
+    return res # ok
+
+def guess_CiDi10(sbkey, t):
+    res = re_PC2(sbkey)
+    for i in range(8):
+        res[not_in_PC2[i]-1] = guess_8bit[t][i]
+    return res # ok
+
+def guess_allsbkey(roundkey, r, t):
+    sbkey = [[]]*16
+    sbkey[r] = roundkey
+    CiDi = guess_CiDi10(roundkey, t)
+    Ci, Di = CiDi[:28], CiDi[28:]
+    for i in range(r+1,r+16):
+        Ci, Di = LR(Ci, Di, i%16)
+        sbkey[i%16] = PC_2(Ci+Di)
+    return sbkey # ok
+
+def long_des_enc(c, k):
+    assert len(c) % 8 == 0
+    res = b''
+    for i in range(0,len(c),8):
+        res += DES_enc(c[i:i+8], k)
+    return res
+
+def try_des(cipher, roundkey):
+    for t in range(256):
+        allkey = guess_allsbkey(roundkey, 10, t)
+        plain = long_des_enc(cipher, allkey[::-1])
+        if plain.startswith(b'NCTF'):
+            print(plain)
+
+if __name__ == "__main__":
+    cipher = b64decode(b'm0pT2YYUIaL0pjdaX2wsxwedViYAaBkZA0Rh3bUmNYVclBlvWoB8VYC6oSUjfbDN')
+    sbkey10 = [0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1]
+    try_des(cipher, sbkey10)
+# b'NCTF{1t_7urn3d_0u7_7h47_u_2_g00d_@_r3v3rs3_1snt}'
